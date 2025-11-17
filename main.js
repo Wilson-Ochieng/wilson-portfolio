314b650099538aca5dfada660ded6d0393427a88
Wilson-Ochieng <wilsonochieng718@gmail.com>
2025-11-17 13:08:39 +0300

    added portfolio

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed( "#typed", {
+
+    strings:["Software Engineer","UI/UX Designer","Frontend Engineer" ],
+    typedSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+}  )
